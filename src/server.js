3e8f429f45fc459931c09d775e754225be322568
Blake <45019373+kinnectusAdmin@users.noreply.github.com>
2024-11-05 15:21:17 -0500

    Remove reference to cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 // app.use(session({
 //   secret: process.env.SESSION_SECRET || 'your-secret-key',
